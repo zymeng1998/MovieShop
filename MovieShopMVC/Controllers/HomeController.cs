2b98f4f83d43c845ccacc7ba644f2043f69d3f86
zymeng1998 <zymeng1998@gmail.com>
2021-10-19 09:42:15 -0700

    view data added

--- MovieShopMVC/Controllers/HomeController.cs
+++ MovieShopMVC/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace MovieShopMVC.Controllers
             // ViewBag and ViewData 
             
             ViewBag.PageTitle = "Top Revenue Movies";
-
+            ViewData["xyz"] = "this is a view data";
             return View(movieCards);
         }
         // https://localhost/home/privacy
