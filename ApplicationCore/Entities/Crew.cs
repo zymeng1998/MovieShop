6965b61ecae08a9ddad81cf08cb6582a6f05cbfd
zymeng1998 <zymeng1998@gmail.com>
2021-10-20 18:07:33 -0700

    class obj created

--- ApplicationCore/Entities/Crew.cs
+++ ApplicationCore/Entities/Crew.cs
@@ -8,5 +8,12 @@ namespace ApplicationCore.Entities
 {
     public class Crew
     {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Gender { get; set; }
+        public string? TmdbUrl { get; set; }
+        public string? ProfilePath { get; set; }
     }
 }
