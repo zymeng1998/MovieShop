8ebe9ec19e2dae173d58c4206680ab9e50b089ae
zymeng1998 <zymeng1998@gmail.com>
2021-10-20 19:27:59 -0700

    create purchase,cs
    dont know how to dissolve unique identifier.

---
+++ ApplicationCore/Entities/Purchase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        /* unique identifier.
+        public  PurchaseNumber { get; set; }
+        */
+        public decimal TotalPrice { get; set; }
+        public DateTime PurchaseDateTime { get; set; }
+        public int MovieId { get; set; }
+
+    }
+}
