e409bea8d036395189452ee678b37e0c957bdf83
zymeng1998 <zymeng1998@gmail.com>
2021-10-27 02:55:44 -0700

    Create AccountController.cs

---
+++ MovieShopMVC/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShopMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+    }
+}
