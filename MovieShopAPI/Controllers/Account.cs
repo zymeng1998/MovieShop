07094c42c3ab9f2ae43275c77b02e3b7037f99bc
zymeng1998 <zymeng1998@gmail.com>
2021-11-11 19:20:58 -0800

    Create Account.cs
    start api auth

---
+++ MovieShopAPI/Controllers/Account.cs
@@ -0,0 +1,25 @@
+﻿using ApplicationCore.ServiceInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieShopMVC.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Account : ControllerBase
+    {
+        private readonly IUserServices _userService;
+        private readonly ICurrentUserService _currentUserService;
+
+        public Account(IUserServices userService, ICurrentUserService currentUserService)
+        {
+            _currentUserService = currentUserService;
+            _userService = userService;
+        }
+    }
+}
